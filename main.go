04dab97b19b85ff5eb6464ab4818a3dd664b001c
Bipol Alam <bipolalam@gmail.com>
2019-05-11 20:48:29 -0400

    env based poll time

--- main.go
+++ main.go
@@ -20,8 +20,9 @@ import (
 )
 
 type options struct {
-	S3BucketName string `long:"s3-bucket-name" env:"S3_BUCKET_NAME" description:"s3 bucket to dump stuff into" required:"true"`
-	MartaAPIKey  string `long:"marta-api-key" env:"MARTA_API_KEY" description:"marta api key" required:"true"`
+	S3BucketName      string `long:"s3-bucket-name" env:"S3_BUCKET_NAME" description:"s3 bucket to dump stuff into" required:"true"`
+	MartaAPIKey       string `long:"marta-api-key" env:"MARTA_API_KEY" description:"marta api key" required:"true"`
+	PollTimeInSeconds int    `long:"poll-time-in-seconds" env:"POLL_TIME_IN_SECONDS" description:"time to poll marta api every second" required:"true"`
 }
 
 func main() {
@@ -47,7 +48,8 @@ func main() {
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	defer cancelFunc()
 
-	poller := worker.New(dump, martaClient, 15*time.Second, logger)
+	logger.Info(fmt.Sprintf("Poll time is %d seconds", opts.PollTimeInSeconds))
+	poller := worker.New(dump, martaClient, time.Duration(opts.PollTimeInSeconds)*time.Second, logger)
 
 	errC := make(chan error, 1)
 	quit := make(chan os.Signal, 1)
