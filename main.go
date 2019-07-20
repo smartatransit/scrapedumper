96097780a73def31da7ae086c20fdd913f93753b
Xander Flood <xander.t.flood@gmail.com>
2019-07-20 12:33:16 -0400

    fix cmd-line args one last time

--- main.go
+++ main.go
@@ -20,11 +20,11 @@ import (
 )
 
 type options struct {
-	OutputLocation    *string `long:"output-location" env:"OUTPUT_LOCATION" description:"local path to output"`
-	DynamoTableName   *string `long:"dynamo-table-name" env:"DYNAMO_TABLE_NAME" description:"dynamo table name"`
-	S3BucketName      *string `long:"s3-bucket-name" env:"S3_BUCKET_NAME" description:"s3 bucket to dump stuff into"`
-	MartaAPIKey       string  `long:"marta-api-key" env:"MARTA_API_KEY" description:"marta api key" required:"true"`
-	PollTimeInSeconds int     `long:"poll-time-in-seconds" env:"POLL_TIME_IN_SECONDS" description:"time to poll marta api every second" required:"true"`
+	OutputLocation    string `long:"output-location" env:"OUTPUT_LOCATION" description:"local path to output"`
+	DynamoTableName   string `long:"dynamo-table-name" env:"DYNAMO_TABLE_NAME" description:"dynamo table name"`
+	S3BucketName      string `long:"s3-bucket-name" env:"S3_BUCKET_NAME" description:"s3 bucket to dump stuff into"`
+	MartaAPIKey       string `long:"marta-api-key" env:"MARTA_API_KEY" description:"marta api key" required:"true"`
+	PollTimeInSeconds int    `long:"poll-time-in-seconds" env:"POLL_TIME_IN_SECONDS" description:"time to poll marta api every second" required:"true"`
 
 	ConfigPath *string `long:"config-path" env:"CONFIG_PATH" description:"An optional file that overrides the default configuration of sources and targets."`
 }
